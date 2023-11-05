021e56c42a7d24fdeac46552e35e72b2c80f354b
Marti <martisalcedo7@hotmail.com>
2023-11-05 23:44:36 +0100

    New trajectory

--- trajectory.py
+++ trajectory.py
@@ -1,50 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def test(final_position):
-    # Define initial and final positions
-    initial_position = 0
+period_T = 0.01
 
-    # Define initial and final velocities
-    initial_velocity = 0
-    final_velocity = 0
+position = 6
+max_velocity = 2
+max_acceleration = 1
 
-    # Define the time duration for the trajectory
-    time_duration = 40  # You can adjust this based on your requirements
+velocity = min(np.sqrt(max_acceleration * position), max_velocity)
 
-    # Calculate the coefficients for the cubic polynomial
-    a = initial_position
-    b = initial_velocity
-    c = (3 * (final_position - initial_position) / (time_duration ** 2)) - (2 * initial_velocity / time_duration) - (final_velocity / time_duration)
-    d = (-2 * (final_position - initial_position) / (time_duration ** 3)) + (initial_velocity / (time_duration ** 2)) + (final_velocity / (time_duration ** 2))
+t_1 = velocity / max_acceleration
+t_2 = position / velocity
+t_3 = t_1 + t_2
 
-    # Define the number of steps
-    num_steps = 400  # You can adjust this based on how many data points you need
+print(t_1)
+print(t_2)
+print(t_3)
 
-    # Calculate position and velocity at each step
-    positions = []
-    velocities = []
+p_1 = (max_acceleration / 2.0) * t_1**2
+p_2 = velocity * (t_2 - t_1) + (max_acceleration / 2.0) * t_1**2
 
-    for t in np.linspace(0, time_duration, num_steps):
-        position = a + b * t + c * (t ** 2) + d * (t ** 3)
-        velocity = b + 2 * c * t + 3 * d * (t ** 2)
-        
-        positions.append(position)
-        velocities.append(velocity)
+time = np.arange(0, t_3+period_T, period_T)
 
-    max_velocity = np.max(np.abs(velocities))
-    velocities = 140.0 * (np.abs(velocities) / max_velocity)
+p = []
+v = []
+for t in time:
+    if t <= t_1:
+        x = (max_acceleration / 2.0) * t**2
+        p.append(x)
+        v.append(np.sqrt(max_acceleration*x*2.0))
+    elif t_1 < t <= t_2:
+        p.append(velocity * (t - t_1) + (max_acceleration / 2.0) * t_1**2)
+        v.append(velocity)
+    elif t_2 < t:
+        x = velocity*(t-t_1)+(max_acceleration/2.0)*t_1**2-(max_acceleration/2.0)*(t-t_2)**2
+        p.append(x)
+        v.append(np.sqrt(velocity**2 - 2.0 * max_acceleration * (x - p_2)))
+    # else:
+    #     raise ValueError
 
-    return np.array(positions), np.array(velocities)
+print(p[-1])
+print(v[-1])
 
-
-if __name__ == "__main__":
-    positions, velocities = test(-450)
-
-    plt.plot(np.diff(positions))
-    plt.grid()
-    plt.show()
-
-    plt.plot(velocities[1:])
-    plt.grid()
-    plt.show()
+plt.plot(time, p)
+plt.plot(time, v)
+plt.grid()
+plt.show()
---
+++ trajectory_steps.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+steps_per_rev = 4076
+
+period_T = 0.01
+
+position = 45
+max_velocity = 30
+max_acceleration = 25
+
+velocity = min(np.sqrt(max_acceleration * position), max_velocity)
+
+t_1 = velocity / max_acceleration
+t_2 = position / velocity
+t_3 = t_1 + t_2
+
+p_1 = (max_acceleration / 2.0) * t_1**2
+p_2 = velocity * (t_2 - t_1) + (max_acceleration / 2.0) * t_1**2
+
+steps = math.floor((position * steps_per_rev) / 360.0)
+
+print(steps)
+
+print(steps * 360.0 / steps_per_rev)
+
+positions_array = np.arange(360.0/steps_per_rev, position, 360.0/steps_per_rev)
+print(positions_array)
+print(len(positions_array))
+
+v = []
+
+for p in positions_array:
+    if p <= p_1:
+        v.append(np.sqrt(max_acceleration*p*2.0))
+    elif p_1 < p <= p_2:
+        v.append(velocity)
+    else:
+        v.append(np.sqrt(velocity**2 - 2.0 * max_acceleration * (p - p_2)))
+
+print(v)
+print(len(v))
+
+plt.plot(v)
+plt.grid()
+plt.show()
